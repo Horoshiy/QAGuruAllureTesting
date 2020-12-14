4cfa8b90018d97ac34c3cf9670e11d055511c5e3
Yuri <yuri@Mac-mini-Yuri.local>
2020-12-14 12:08:06 +0300

    add check tests results

--- src/test/java/CreateIssueInGithubRepositoryTests.java
+++ src/test/java/CreateIssueInGithubRepositoryTests.java
@@ -1,36 +1,35 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.example.hume.allure.Credentials;
 import io.qameta.allure.Step;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.IOException;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.*;
 import static org.openqa.selenium.By.linkText;
 
-
+@TestInstance(PER_CLASS)
 public class CreateIssueInGithubRepositoryTests {
     private final static String GITHUB_URL = "https://github.com";
+    private final static String issueTitle = "Title of Issue";
     private static String user;
     private static String password;
     private static String repository;
 
-    @BeforeEach
-    void getCredentials() throws IOException {
+    @BeforeAll
+    void credentialsAndSelenideConfiguration() throws IOException {
         Credentials credentials = new Credentials();
         user = credentials.getUserName();
         password = credentials.getPassword();
         repository = user + "/QAGuruAllureTesting";
-    }
 
-    @BeforeEach
-    void selenideSettings() {
         Configuration.startMaximized = true;
     }
 
@@ -47,7 +46,7 @@ public class CreateIssueInGithubRepositoryTests {
         $("#login_field").sendKeys(user);
         $("#password").sendKeys(password);
         $(byName("commit")).submit();
-        $("#issue_title").sendKeys("Issue");
+        $("#issue_title").sendKeys(issueTitle);
         $(withText("Assignees")).click();
         $("#assignee-filter-field").val(user).pressEnter();
         $(withText("Assignees")).click();
@@ -55,13 +54,14 @@ public class CreateIssueInGithubRepositoryTests {
         $(withText("Labels")).click();
         $("#label-filter-field").val("bug").pressEnter();
         $(withText("Labels")).click();
-        $(withText("Labels")).click();
         $(withText("Submit new issue")).click();
+        $(".application-main").shouldHave(text(issueTitle));
     }
 
     @Test
     void createIssueWithStepsTest() {
-        step("Открываем главную страницу", () -> open(GITHUB_URL));
+        step("Открываем главную страницу", () ->
+                open(GITHUB_URL));
         step("Ищем репозиторий " + repository, (step) -> {
             step.parameter("name", repository);
             $(".header-search-input").click();
@@ -72,15 +72,19 @@ public class CreateIssueInGithubRepositoryTests {
             step.parameter("name", repository);
             $(linkText(repository)).click();
         });
-        step("Переходим в раздел Issues", () -> $(withText("Issues")).click());
-        step("Жмём на кнопу 'Создать Issue'", () -> $(withText("New issue")).click());
-        step("Переходим на страницу авторизации", () -> $(withText("Already on GitHub?")).$(linkText("Sign in")).click());
+        step("Переходим в раздел Issues", () ->
+                $(withText("Issues")).click());
+        step("Жмём на кнопу 'Создать Issue'", () ->
+                $(withText("New issue")).click());
+        step("Переходим на страницу авторизации", () ->
+                $(withText("Already on GitHub?")).$(linkText("Sign in")).click());
         step("Заполняем авторизационную форму", () -> {
             $("#login_field").sendKeys(user);
             $("#password").sendKeys(password);
             $(byName("commit")).submit();
         });
-        step("Заполняем поле Title в Create New Issue", () -> $("#issue_title").sendKeys("Issue"));
+        step("Заполняем поле Title в Create New Issue", () ->
+                $("#issue_title").sendKeys(issueTitle));
         step("Заполняем Assignees", () -> {
             $(withText("Assignees")).click();
             $("#assignee-filter-field").val(user).pressEnter();
@@ -91,12 +95,15 @@ public class CreateIssueInGithubRepositoryTests {
             $("#label-filter-field").val("bug").pressEnter();
             $(withText("Labels")).click();
         });
-        step("Жмем на кнопу Создания Issue", () -> $(withText("Submit new issue")).click());
+        step("Жмем на кнопу Создания Issue", () -> {
+            $(withText("Submit new issue")).click();
+            $(".application-main").shouldHave(text(issueTitle));
+        });
     }
 
     @Test
     public void createIssueWithAnnotationsTest() {
-        final BaseSteps steps = new BaseSteps();
+        BaseSteps steps = new BaseSteps();
         steps.openMainPage();
         steps.searchForRepository(repository);
         steps.goToRepository(repository);
@@ -104,10 +111,10 @@ public class CreateIssueInGithubRepositoryTests {
         steps.pressButtonCreateIssue();
         steps.goToAuthorizationPage();
         steps.fillAuthorizationForm();
-        steps.fillTitleFieldInCreateNewIssueForm();
+        steps.fillTitleFieldInCreateNewIssueForm(issueTitle);
         steps.fillAssigneesInCreateNewIssueForm();
         steps.fillLabelsInCreateNewIssueForm();
-        steps.pressSubmitButtonInCreateNewIssueForm();
+        steps.pressSubmitButtonInCreateNewIssueFormAndCheckTitleExists(issueTitle);
     }
 
     @AfterEach
@@ -157,8 +164,8 @@ public class CreateIssueInGithubRepositoryTests {
         }
 
         @Step("Заполняем поле Title в Create New Issue")
-        public void fillTitleFieldInCreateNewIssueForm() {
-            $("#issue_title").sendKeys("Issue");
+        public void fillTitleFieldInCreateNewIssueForm(String title) {
+            $("#issue_title").sendKeys(title);
         }
 
         @Step("Заполняем Assignees в Create New Issue")
@@ -176,8 +183,9 @@ public class CreateIssueInGithubRepositoryTests {
         }
 
         @Step("Жмем на кнопу Создания Issue")
-        public void pressSubmitButtonInCreateNewIssueForm() {
+        public void pressSubmitButtonInCreateNewIssueFormAndCheckTitleExists(String title) {
             $(withText("Submit new issue")).click();
+            $(".application-main").shouldHave(text(title));
         }
     }
 }
